50a5dd43c6f6192d0b71231f62ee0ceea2539908
Olafur Pall Geirsson <lgeirsson@twitter.com>
2020-05-27 14:21:47 +0000

    Download binaries

--- build-tar.sh
+++ build-tar.sh
@@ -2,36 +2,16 @@
 set -eux
 VERSION="$1"
 TAG="v$1"
-METALS_VERSION=$2
+FASTPASS_VERSION=$2
 BLOOP_VERSION=$3
 rm -rf bin
 mkdir bin
-./link-fastpass.sh $METALS_VERSION
-./link-bloop.sh $BLOOP_VERSION
-# curl -Lo bin/bloop https://github.com/scalacenter/bloop/releases/download/v$BLOOP_VERSION/bloop-x86_64-apple-darwin
+curl -Lo bin/fastpass https://github.com/scalameta/fastpass/releases/download/v$FASTPASS_VERSION/fastpass-x86_64-apple-darwin
+curl -Lo bin/bloop https://github.com/scalacenter/bloop/releases/download/v$BLOOP_VERSION/bloop-x86_64-apple-darwin
+chmod +x bin/fastpass
+chmod +x bin/bloop
 TAR=fastpass.tar.gz
 file bin/fastpass
 file bin/bloop
 tar -czvf $TAR bin/
 hub release create -a $TAR -m "Fastpass $TAG" $TAG
-FASTPASS_URL=https://github.com/olafurpg/homebrew-fastpass/releases/download/$TAG/$TAR
-SHA=$(curl -L $FASTPASS_URL | shasum -a 256 | awk '{ print $1 }')
-
-cat > fastpass.rb <<EOF
-require 'formula'
-
-class Fastpass < Formula
-  desc "Metals $VERSION"
-  homepage ""
-  version "$VERSION"
-  url "$FASTPASS_URL"
-  sha256 "$SHA"
-  depends_on "scalacenter/bloop/bloop"
-  def install
-    bin.install "fastpass"
-  end
-end
-EOF
-git add .
-git commit -am "Release $TAG"
-git push origin master
